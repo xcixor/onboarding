80ab5a0402fd3547cde1976fd13ea7c5f7561423
xcixor <pndungu54@gmail.com>
2024-05-07 13:40:01 +0300

    Add getEventAttendees action and attendees page for admin events
    Create getEventAttendees action to fetch attendees for a specific event
    and add attendees page for admin events to display attendee information.

---
+++ app/dashboard/(routes)/profile/admin/events/[eventId]/attendees/page.tsx
@@ -0,0 +1,31 @@
+import { getEventAttendees } from "@/actions/admin/get-event-attendees";
+import { cn } from "@/lib/utils";
+import React from "react";
+
+type Props = {
+  params: {
+    eventId: string;
+  };
+};
+
+const page = async (props: Props) => {
+  const attendees = await getEventAttendees(props.params.eventId);
+  return (
+    <div className="space-y-4 p-6">
+      <h1 className="text-2xl">Attendees</h1>
+      {attendees.map((attendee) => (
+        <div
+          key={attendee.id}
+          className={cn("flex gap-2 bg-slate-100 p-4", {
+            "bg-slate-300": attendees.indexOf(attendee) % 2 !== 0,
+          })}
+        >
+          <p>{attendee.name}</p>
+          <p>{attendee.email}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default page;
--- app/dashboard/(routes)/profile/admin/events/[eventId]/page.tsx
+++ app/dashboard/(routes)/profile/admin/events/[eventId]/page.tsx
@@ -2,6 +2,7 @@ import { getEventById } from "@/actions/admin/get-event-by-id";
 import { getLatestFileMetaData } from "@/actions/get-latest-file-metadata";
 import CreateEventForm from "@/components/admin/events/CreateEventForm";
 import EventPosterForm from "@/components/admin/events/EventPosterForm";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import React from "react";
 
@@ -17,6 +18,9 @@ const page = async (props: Props) => {
   const gcpData = await getLatestFileMetaData(resource.id);
   return (
     <div className="h-full p-6">
+      <Link href={`/dashboard/profile/admin/events/${resource.id}/attendees`} className="bg-primary text-white p-2 inline-block rounded-sm">
+        Attendees
+      </Link>
       <p>Event ID: {resource.slug}</p>
       <CreateEventForm
         title={resource.title}
